bdb50b7ae48fd03f62fd1edf06e0df7a6383d3ea
karma <karmatashi954@gmail.com>
2022-08-01 15:58:42 +0545

    browse compositional collection view

--- SpotifyClone/Controllers/Core/HomeViewController.swift
+++ SpotifyClone/Controllers/Core/HomeViewController.swift
@@ -8,16 +8,83 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private var collectionView = UICollectionView(frame: .zero,
+                                                 collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ in
+        return HomeViewController.createSectionLayout(section: sectionIndex)
+    })
+    
+    private let spinner: UIActivityIndicatorView = {
+       let spinner = UIActivityIndicatorView()
+        spinner.tintColor = .label
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Browse"
         view.backgroundColor = .systemBackground
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettings))
+        configureCollectionView()
+        view.addSubview(spinner)
         fetchData()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    private func configureCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self,
+                                forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+    }
+    
+    private static func createSectionLayout(section: Int) -> NSCollectionLayoutSection {
+        // Item
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)))
+        
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        
+        // Group
+        // Vertical group in horizontal group
+        let verticalGroup = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(390)),
+            subitem: item,
+            count: 3)
+        
+        let horizontalGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(0.9),
+                heightDimension: .absolute(390)),
+            subitem: verticalGroup,
+            count: 1)
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: horizontalGroup)
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        return section
+    }
+    
     private func fetchData() {
+        // New Releases
+        // Featured playlist
+        // Recommended Tracks
+        
+        
+        
+        
+        
 //        APICaller.shared.getNewReleases { [weak self] result in
 //            switch result{
 //            case .success(let result):
@@ -76,5 +143,24 @@ class HomeViewController: UIViewController {
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
     }
+    
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+    
+    
     
 }
