11f0ca1fd197e493a9eced52cbac2915c71ae58e
karma <karmatashi954@gmail.com>
2022-08-16 10:57:25 +0545

    implementation of signout feature

--- SpotifyClone/Controllers/Others/SettingsViewController.swift
+++ SpotifyClone/Controllers/Others/SettingsViewController.swift
@@ -62,18 +62,24 @@ class SettingsViewController: UIViewController {
     
     private func signOutTapped() {
         AuthManager.shared.signOut { [weak self] signedOut in
-            if signedOut {
-                DispatchQueue.main.async {
-                    
-                    let navVC = UINavigationController(rootViewController: WelcomeViewController())
-                    navVC.navigationBar.prefersLargeTitles = true
-                    navVC.viewControllers.first?.navigationItem.largeTitleDisplayMode = .always
-                    navVC.modalPresentationStyle = .fullScreen
-                    self?.present(navVC, animated: true,completion: {
-                        self?.navigationController?.popToRootViewController(animated: false)
-                    })
+            let alert = UIAlertController(title: "Sign Out",
+                                          message: "Are you sure you want to sign out?",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { _ in
+                if signedOut {
+                    DispatchQueue.main.async {
+                        let navVC = UINavigationController(rootViewController: WelcomeViewController())
+                        navVC.navigationBar.prefersLargeTitles = true
+                        navVC.viewControllers.first?.navigationItem.largeTitleDisplayMode = .always
+                        navVC.modalPresentationStyle = .fullScreen
+                        self?.present(navVC, animated: true,completion: {
+                            self?.navigationController?.popToRootViewController(animated: false)
+                        })
+                    }
                 }
-            }
+            }))
+            present(alert, animated: true)
         }
     }
     
